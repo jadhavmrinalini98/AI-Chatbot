bac2c5398220da85c61db87061d390808987b920
jadhavmrinalini98 <114711523+jadhavmrinalini98@users.noreply.github.com>
2025-02-15 16:59:34 -0500

    ChatBotStart component created

---
+++ ai-chat-bot/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ChatBotStart from './Components/ChatBotStart'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <ChatBotStart/>
+
+    </div>
+  )
+}
+
+export default App
